62d1b8a72161e0e3366ba225f6e138ee559a4367
Oleg <oleg20502@gmail.com>
2020-09-22 15:11:13 +0300

    deleted comments

--- Lab3/smile.py
+++ Lab3/smile.py
@@ -9,7 +9,6 @@ import pygame
 from pygame.draw import *
 pygame.init()
 
-
 FPS = 30
 screen = pygame.display.set_mode((800, 800))
 
@@ -21,10 +20,6 @@ circle(screen, (255, 0, 0), (465, 350), 25)
 circle(screen, (0, 0, 0), (465, 350), 12)
 line(screen, (0, 0, 0), [320, 300], [380, 330], 15)
 line(screen, (0, 0, 0), [420, 330], [480, 300], 15)
-#rect(screen, (120, 100, 255), (100, 100, 200, 200), 15)
-#polygon(screen, (255, 255, 0), [(100,100), (200,50), (300,100), (200, 200), (100,100)])
-#polygon(screen, (0, 0, 255), [(100,100), (200,50),(300,100), (100,100)], 5)
-
 
 pygame.display.update()
 clock = pygame.time.Clock()
