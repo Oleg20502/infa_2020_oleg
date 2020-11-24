eb4930fa5ecc747d7d9b7b9c0482044d2f41aea5
Oleg20502 <oleg20502@gmail.com>
2020-11-24 12:49:57 +0300

    Removed comment bugs

--- Lab3/1_draw.py
+++ Lab3/1_draw.py
@@ -16,10 +16,8 @@ screen = pygame.display.set_mode((400, 400))
 
 rect(screen, (255, 0, 255), (100, 100, 200, 200))
 rect(screen, (0, 0, 255), (100, 100, 200, 200), 5)
-#polygon(screen, (255, 255, 0), [(100,100), (200,50),
-                               (300,100), (100,100)])
-#polygon(screen, (0, 0, 255), [(100,100), (200,50),
-                               (300,100), (100,100)], 5)
+#polygon(screen, (255, 255, 0), [(100,100), (200,50), (300,100), (100,100)])
+#polygon(screen, (0, 0, 255), [(100,100), (200,50), (300,100), (100,100)], 5)
 circle(screen, (0, 255, 0), (200, 175), 50)
 circle(screen, (255, 255, 255), (200, 175), 50, 5)
 
@@ -30,7 +28,12 @@ finished = False
 while not finished:
     clock.tick(FPS)
     for event in pygame.event.get():
+        print(event)
         if event.type == pygame.QUIT:
             finished = True
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 4:
+                print(1)
+                print(event)
 
 pygame.quit()
